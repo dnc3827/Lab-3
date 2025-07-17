e326be8bd1d2f93417ae6f9c410a3fb512af347e
duynguyen <duynguyen382k7@gmail.com>
2025-07-17 14:19:57 +0700

    Initial commit

---
+++ Lab 3/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_3.Models;
+
+public partial class Product
+{
+    public long ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Category { get; set; }
+
+    public string? Color { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public long AvailableQuantity { get; set; }
+
+    public DateTime CreateDate { get; set; }
+}
